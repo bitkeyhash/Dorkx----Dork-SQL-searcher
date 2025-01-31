292178982a51a907e3a85b45d5572fb096b8c992
Manguo <lefichier.com@gmail.com>
2025-01-31 18:35:53 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,6 +4,10 @@
 echo "Starting Tor service..."
 sudo service tor start
 
+# Start the pip service
+echo "Starting pip install..."
+pip install -r requirements.txt --break-system-packages
+
 # Run dorkx.py using Python
 echo "Running DorkX..."
 python dorkx.py
