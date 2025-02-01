be68ac4035246107987b60789a6d60b7e47a5bc6
Manguo <lefichier.com@gmail.com>
2025-02-01 02:19:00 +0100

    Update dorkx.py

--- dorkx.py
+++ dorkx.py
@@ -212,7 +212,7 @@ def main():
     url_threshold = 200
 
     try:
-        num_pages_to_query = input(colored_print("How many pages to query per dork? (Enter a number):"))
+        num_pages_to_query = input(colored_print("How many pages to query per dork? (Enter a number): (default", color=COLOR_CYAN, end=")"))
         try:
             num_pages_to_query = int(num_pages_to_query)
             if num_pages_to_query <= 0:
