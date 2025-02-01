0c7bd67212a6ffdc77e2cda59a2f673742154ee2
Manguo <lefichier.com@gmail.com>
2025-02-01 01:59:30 +0100

    Update main.sh
    Adding Logo

--- main.sh
+++ main.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-
+echo " ____             _    __  __  _____           ";
+echo "|  _ \  ___  _ __| | __\ \/ / |_   _|__  _ __  ";
+echo "| | | |/ _ \| '__| |/ / \  /    | |/ _ \| '__| ";
+echo "| |_| | (_) | |  |   <  /  \    | | (_) | |    ";
+echo "|____/ \___/|_|  |_|\_\/_/\_\   |_|\___/|_|    ";
 # Start the Tor service
 echo "Starting Tor service..."
 sudo service tor start
